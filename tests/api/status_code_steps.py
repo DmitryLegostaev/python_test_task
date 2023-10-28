88a2a7bdf2be8778cb9731ed617dbfc4555c6b6a
Dmitry Legostaev <24895280+DmitryLegostaev@users.noreply.github.com>
2023-10-28 19:29:08 +0300

    refactor: changed assertion message of status code check

--- tests/api/status_code_steps.py
+++ tests/api/status_code_steps.py
@@ -3,5 +3,5 @@ import allure
 
 @allure.step("Check status code")
 def status_code_check(actual_status_code: int, expected_status_code: int):
-    assert actual_status_code == expected_status_code, (f"Expected status code: {expected_status_code}. Actual "
-                                                        f"status code: {actual_status_code}")
+    assert actual_status_code == expected_status_code, (f"Actual status code: {actual_status_code}. Expected "
+                                                        f"status code: {expected_status_code}")
